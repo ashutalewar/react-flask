7db6a12841be44a98cdb4407ac657982197a882c
ashutalewar <ashutalewar@gmail.com>
2018-01-16 01:24:08 +0530

    adding api call to flask

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template
+import time
+
+
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def react():
   return render_template('index_flask.html')
 
+@app.route('/api', methods=['GET'])
+def api():
+	time.sleep(5)
+	return "api call working!!!!!!", 200
+
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
+
+
+
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port=3000)
+  app.run(debug=True, host='0.0.0.0', port=5000)
 
--- js/Page1.js
+++ js/Page1.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Request from 'react-http-request';
+import axios from 'axios';
 
 /////////////// material-ui components /////////////
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
@@ -10,21 +12,34 @@ export default class Page1 extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      random : false
+      random : false,
+      api_data : ''
     };
 
   }
 
   startSomething = ()  => {
-  	console.log("changing button state",this.state.random)
+  	console.log("changing button state - ",this.state.random)
     this.setState({random : true})
+    this.setState({api_data : "Waiting for response"})
+
+    axios.get('http://localhost:5000/api')
+      .then(res => {
+        console.log(res.data)
+        this.setState({api_data : res.data})
+        this.setState({random : false})
+      })
+
   }
 
   render() {
     return (
+      <div>
     	<MuiThemeProvider>
-          <RaisedButton label="Start" primary={true} onClick={this.startSomething} disabled={this.state.random} />
-        </MuiThemeProvider>
+        <RaisedButton label="Start" primary={true} onClick={this.startSomething} disabled={this.state.random} />
+      </MuiThemeProvider>
+      <h2>{this.state.api_data}</h2>
+      </div>
     );
   }
 }
